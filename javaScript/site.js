75a1ad1fb7ef5856df73ca6e0237d6ec9df912d8
Handrich <500198871@fvtc.edu>
2024-02-20 19:50:56 -0600

    master coder

--- javaScript/site.js
+++ javaScript/site.js
@@ -5,3 +5,5 @@ const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 const welcome = document.querySelector('#welcome')
 welcome.textContent = isMorning ? "Good Morning" : isAfternoon ? "Good Afternoon" : "Good Evening"
+
+localStorage.setItem("It's a secret to everybody.",'This is a zelda reference')
